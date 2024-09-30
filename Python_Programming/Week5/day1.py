aca540cd24502755d7050702e2c9e537db749fad
joshuayeyo <chiikawayeyo@gmail.com>
2024-09-30 14:28:22 +0900

    Add 2차원 리스트의 동적생성 예제코드

--- Python_Programming/Week5/day1.py
+++ Python_Programming/Week5/day1.py
@@ -126,3 +126,27 @@ print(letters)
 
 numbers = [x+y for x in ['a','b','c'] for y in ['x','y','z']]
 print(numbers)
+
+# 2차원 리스트
+s = [
+    [ 1, 2, 3, 4, 5 ],
+    [ 6, 7, 8, 9, 10 ],
+    [ 11, 12, 13, 14, 15 ]
+]
+
+print(s)
+
+# 2차원 리스트의 동적생성
+rows = 3
+cols = 5
+s = []
+for row in range(rows):
+    s += [[0]*cols] # 2차원 리스트끼리 합쳐진다.
+    s += [0*cols]   # 1차원 리스트끼리 합쳐진다.
+print("s =", s)
+
+# 리스트 함축 이용
+rows = 3
+cols = 5
+s = [ (0 * cols) for row in range(rows)]
+print("s =", s)
