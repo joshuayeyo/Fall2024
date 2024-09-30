ae8cc87f1137be16645d0688694a1623f6fa1cd7
joshuayeyo <chiikawayeyo@gmail.com>
2024-09-30 13:22:52 +0900

    Add 내장함수 예제코드

--- Python_Programming/Week5/day1.py
+++ Python_Programming/Week5/day1.py
@@ -2,3 +2,9 @@ heroes = []
 heroes.append("아이언맨")
 heroes.append("토르")
 print(heroes)
+
+# built-in function
+numbers = [10, 20, 30, 40, 50]
+print("합=", sum(numbers))        # 항목의 합계를 계산한다.
+print("최대값=", max(numbers))     # 가장 큰 항목을 반환한다.
+print("최소값=", min(numbers))     # 가장 작은 항목을 반환한다.
