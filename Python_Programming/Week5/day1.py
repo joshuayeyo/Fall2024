172bbd0991eb7f6d43c700d27eeb72a7b9636cba
joshuayeyo <chiikawayeyo@gmail.com>
2024-09-30 14:18:10 +0900

    Add 리스트 함축 if문을 사용하여 조건추가 additional 예제코드

--- Python_Programming/Week5/day1.py
+++ Python_Programming/Week5/day1.py
@@ -115,3 +115,14 @@ for x in range(10):
     if x % 2 == 0:
         sqrs2.append(x*x)
 print(sqrs2)
+
+prices = [135, -545, 922, 356, -992, 217]
+mprices = [i if i >0 else 0 for i in prices]
+print(mprices)
+
+words = ["All", "good", "things", "must", "come", "to", "an", "end."]
+letters = [w[0] for w in words]
+print(letters)
+
+numbers = [x+y for x in ['a','b','c'] for y in ['x','y','z']]
+print(numbers)
