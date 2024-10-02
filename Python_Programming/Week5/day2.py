ab8bdc073e1f58b8bf598886d4e224ad5ece57c2
joshuayeyo <chiikawayeyo@gmail.com>
2024-10-02 14:17:19 +0900

    딕셔너리 함축 예제코드

--- Python_Programming/Week5/day2.py
+++ Python_Programming/Week5/day2.py
@@ -117,3 +117,15 @@ for key in capitals:
 ###　方法２
 for key, value in capitals.items():
     print(key,":",value)
+
+## 딕셔너리 함축
+values = [1, 2, 3, 4, 5, 6]
+dic = { x: x**2 for x in values if x%2==0}
+print(dic)
+
+dic = {i:str(i) for i in [1,2,3,4,5]}
+print(dic)
+
+fruits = ["apple", "orange", "banana"]
+dic = {f:len(f) for f in fruits}
+print(dic)
