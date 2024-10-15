aa2eddf9848f724b8cbf4d138ef6760f06ea8f08
joshuayeyo <chiikawayeyo@gmail.com>
2024-10-15 13:43:46 +0900

    Ch.01 Lab

---
+++ Python_Programming/Lab/Ch.01/Lab04.py
@@ -0,0 +1,14 @@
+# page 49
+## 터틀 그래픽으로 삼각형을 그려보겠습니다.
+import turtle
+
+t= turtle.Turtle()
+t.shape("turtle")
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+
+turtle.mainloop()
+turtle.bye()
